c9a4fd8a44920c17e9e5aaa61af33bca477b1412
CELUSION-LAP93\Umair Alam Shaikh <umairalam.shaikh@celusion.com>
2023-11-01 16:40:46 +0530

    [E-2] Add bussiness & web

---
+++ NetShopeWeb/EfContext/dbContext.cs
@@ -0,0 +1,37 @@
+﻿using NetShopeBusiness;
+using NetShopeBusiness.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NetShopeWeb.EfContext
+{
+    public class dbContext:DbContext
+    {
+        public dbContext():base("SqlServerContext")
+        {
+
+        }
+        public virtual DbSet<admin_Employee> admin_Employee { get; set; }
+        public virtual DbSet<admin_Login> admin_Login { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<genMainSlider> genMainSliders { get; set; }
+        public virtual DbSet<genPromoRight> genPromoRights { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Payment> Payments { get; set; }
+        public virtual DbSet<PaymentType> PaymentTypes { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<RecentlyView> RecentlyViews { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<ShippingDetail> ShippingDetails { get; set; }
+        public virtual DbSet<SubCategory> SubCategories { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+        public virtual DbSet<Wishlist> Wishlists { get; set; }
+        public virtual DbSet<ProductPicture> ProductPictures { get; set; }
+    }
+}
---
+++ NetShopeWeb/Migrations/202311011045586_Add_Picture_list.cs
@@ -0,0 +1,33 @@
+﻿namespace NetShopeWeb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_Picture_list : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductPictures",
+                c => new
+                    {
+                        ProductPictureID = c.Int(nullable: false, identity: true),
+                        PicturePath = c.String(),
+                        ProductID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductPictureID)
+                .ForeignKey("dbo.Products", t => t.ProductID, cascadeDelete: true)
+                .Index(t => t.ProductID);
+            
+            DropColumn("dbo.Products", "PicturePath");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "PicturePath", c => c.String());
+            DropForeignKey("dbo.ProductPictures", "ProductID", "dbo.Products");
+            DropIndex("dbo.ProductPictures", new[] { "ProductID" });
+            DropTable("dbo.ProductPictures");
+        }
+    }
+}
